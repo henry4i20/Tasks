029154a45f762fc329c3799d7e26e6d088789c45
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-08-02 16:58:41 -0300

    rapaix

--- landingPage/script.js
+++ landingPage/script.js
@@ -4,17 +4,17 @@ var imgatual="img/buzz.jpg";
 var imganterior="img/ponto-de-interrogacao.png";
 
 function trocar(){
-    document.getElementById("ref").src=imgatual;
-    let aux=imgatual;
-    imgatual=imganterior;
-    imganterior=aux;
+   var link="https://open.spotify.com/intl-pt/album/6PBZN8cbwkqm1ERj2BGXJ1?si=cxXtjxLQQpqAWW0TziHUiQ";
+   window.open(link,'_blank') ; 
 }
 function ConferirMensagem(){
     var nome=document.getElementById("nome").value;
     var mensagem=document.getElementById("mensagem").value;
+    var confDiv=document.getElementById("rightDiv")
 
 nomeGlobal=nome
 msgGlobal=mensagem
+confDiv.style.display="flex"
 
 document.getElementById("confNome").textContent=nome;
 document.getElementById("confMsg").textContent=mensagem;
